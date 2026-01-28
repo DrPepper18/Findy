c52de8003cd51799831ac842fe9d61b4ec10f1d1
DrPepper18 <dbusygin70@gmail.com>
2026-01-28 11:48:03 +0300

    feat: add refresh token tests refactor: explicit argument types in tests

--- backend/tests/integration/test_auth_features.py
+++ backend/tests/integration/test_auth_features.py
@@ -1,10 +1,26 @@
 import pytest
+from httpx import AsyncClient
 from app.utils.date_functions import calculate_birthdate
 
 users = [
-    {"email": "", "password": "", "name": "", "birthdate": None},
-    {"email": "Late bird", "password": "ihavetime", "name": "Late bird", "birthdate": calculate_birthdate(20).isoformat()},
-    {"email": "naughtykid@mail.ru", "password": "im18iswear", "name": "Kid", "birthdate": calculate_birthdate(17).isoformat()},
+    {
+        "email": "",
+        "password": "", 
+        "name": "", 
+        "birthdate": None
+    },
+    {
+        "email": "Late bird", 
+        "password": "ihavetime", 
+        "name": "Late bird", 
+        "birthdate": calculate_birthdate(20).isoformat()
+    },
+    {
+        "email": "naughtykid@mail.ru", 
+        "password": "im18iswear", 
+        "name": "Kid", 
+        "birthdate": calculate_birthdate(17).isoformat()
+    },
 ]
 
 @pytest.mark.parametrize("user, status_code", [
@@ -13,13 +29,13 @@ users = [
     (users[2], 201),    # OK == 201
 ])
 @pytest.mark.asyncio
-async def test_auth_register(client, user, status_code):
+async def test_auth_register(client: AsyncClient, user, status_code):
     response = await client.post('/api/v1/auth/register', json=user)
     assert response.status_code == status_code
 
 
 @pytest.mark.asyncio
-async def test_auth_register_duplicate(client):
+async def test_auth_register_duplicate(client: AsyncClient):
     user = users[2]
     await client.post('/api/v1/auth/register', json=user)
     response = await client.post('/api/v1/auth/register', json=user)
@@ -37,7 +53,7 @@ async def test_auth_register_duplicate(client):
     ({"email": "good@user.ru", "password": "password123", "attempt": "password123"}, 200)
 ])
 @pytest.mark.asyncio
-async def test_auth_login(client, user, status_code):
+async def test_auth_login(client: AsyncClient, user, status_code):
     if status_code in [401, 200]:
         await client.post('/api/v1/auth/register', json={
             "email": user["email"], 
@@ -53,3 +69,39 @@ async def test_auth_login(client, user, status_code):
     })
     
     assert response.status_code == status_code
+
+
+@pytest.mark.asyncio
+async def test_tokens_refresh(client: AsyncClient):
+    user = {
+        "email": "good@user.ru", 
+        "password": "password123",
+        "name": "TestUser", 
+        "birthdate": calculate_birthdate(20).isoformat()
+    }
+    response = await client.post('/api/v1/auth/register', json=user)
+    refresh_token = response.cookies.get("refresh")
+    access_token = response.json()["token"]
+    assert refresh_token
+    assert access_token
+
+    client.cookies.set("refresh", refresh_token)
+    response = await client.get('/api/v1/auth/refresh')
+    new_refresh_token = response.cookies.get("refresh")
+    new_access_token = response.json()["token"]
+    assert new_refresh_token
+    assert new_access_token
+
+    # I still didn't add refresh token rotation.
+    # So theoretically every generated RT
+    # that isn't out of date will be valid. 
+    # But OK i think nevermind rn.
+
+    client.cookies.set("refresh", new_refresh_token)
+    response = await client.get('/api/v1/auth/', headers={'Authorization': f"Bearer {new_access_token}"})
+    user_data = response.json()
+    assert user_data["name"] == user["name"]
+    
+    client.cookies.clear()
+    response = await client.get('/api/v1/auth/refresh')
+    assert response.status_code == 401
---
+++ backend/tests/unit/test_utils_security.py
@@ -0,0 +1,19 @@
+from app.utils.security import *
+
+
+def test_jwt_token_cycle(email: str = "test@gmail.com"):
+    token = create_jwt_token(email=email)
+    payload = verify_access_token(token=token)
+    assert payload["sub"] == email
+
+
+def test_tokens_pair_cycle(email: str = "test@gmail.com"):
+    tokens = create_both_tokens(email=email)
+    payload_access = verify_access_token(token=tokens["access"])
+    payload_refresh = verify_refresh_token(token=tokens["refresh"])
+    assert payload_access["sub"] == payload_refresh["sub"]
+
+
+def test_password_hash_cycle(password: str = "password123"):
+    password_hash = create_password_hash(password=password)
+    assert is_password_correct(password=password, password_hash=password_hash)
